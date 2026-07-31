e1102ba17b6c06319f27ce563984dc1d3dc52eb7
pannstonnam <pannstonnam@gmail.com>
2026-07-31 09:40:15 +0700

    Update DRIVE_FOLDER_NAME to version 3

--- nick_agent.py
+++ nick_agent.py
@@ -21,7 +21,7 @@ from tee_agent import (
     read_knowledge_base,
 )
 
-DRIVE_FOLDER_NAME = "Nick_AI_Agent_v2"
+DRIVE_FOLDER_NAME = "Nick_AI_Agent_v3"
 
 DEFAULT_PORTFOLIO = """# พอร์ตโฟลิโอเริ่มต้น
 - เงินสด: 100%
